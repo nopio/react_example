250d8926eeb0f58b05a0af9551825573be961578
Piotr Jaworski <piotr.jaworski@nopio.com>
2016-08-11 10:43:32 +0200

    Updated new_form.js.jsx

--- app/assets/javascripts/components/new_form.js.jsx
+++ app/assets/javascripts/components/new_form.js.jsx
@@ -1,26 +1,29 @@
 var NewForm = React.createClass({
+  propTypes: {
+    name: React.PropTypes.string,
+    event_date: React.PropTypes.string,
+    place: React.PropTypes.string,
+    description: React.PropTypes.string
+  },
+  getInitialState: function() {
+    return {
+      name: '',
+      place: '',
+      event_date: '',
+      description: ''
+    }
+  },
   handleAdd: function(e) {
     e.preventDefault();
-    var name = ReactDOM.findDOMNode(this.refs.name);
-    var place = ReactDOM.findDOMNode(this.refs.place);
-    var event_date = ReactDOM.findDOMNode(this.refs.event_date);
-    var description = ReactDOM.findDOMNode(this.refs.description);
     var self = this;
-    if (!!name && !!place && !!event_date && !!description) {
+    if (this.validForm()) {
       $.ajax({
         url: '/api/events',
         method: 'POST',
-        data: { event: { name: name.value,
-                         place: place.value,
-                         event_date: event_date.value,
-                         description: description.value
-                       }},
+        data: { event: self.state },
         success: function(data) {
           self.props.handleAdd(data);
-          name.value = '';
-          place.value = '';
-          event_date.value = '';
-          description.value = '';
+          self.setState(self.getInitialState());
         },
         error: function(xhr, status, error) {
           alert('Cannot add a new record: ', error);
@@ -30,6 +33,19 @@ var NewForm = React.createClass({
       alert('Please fill all fields.');
     }
   },
+  validForm: function() {
+    if (this.state.name && this.state.place &&
+        this.state.event_date && this.state.description) {
+      return true;
+    } else {
+      return false;
+    }
+  },
+  handleChange: function(e) {
+    var input_name = e.target.name;
+    var value = e.target.value;
+    this.setState({ [input_name] : value });
+  },
   render: function() {
     return(
       <form className="form-inline" onSubmit={this.handleAdd}>
@@ -38,28 +54,36 @@ var NewForm = React.createClass({
                  className="form-control"
                  name="name"
                  placeholder="Name"
-                 ref="name" />
+                 ref="name"
+                 value={this.state.name}
+                 onChange={this.handleChange} />
         </div>
         <div className="form-group">
           <input type="text"
                  className="form-control"
                  name="place"
                  placeholder="Place"
-                 ref="place" />
+                 ref="place"
+                 value={this.state.place}
+                 onChange={this.handleChange} />
         </div>
         <div className="form-group">
           <input type="date"
                  className="form-control"
                  name="event_date"
                  placeholder="Event date"
-                 ref="event_date" />
+                 ref="event_date"
+                 value={this.state.event_date}
+                 onChange={this.handleChange} />
         </div>
         <div className="form-group">
           <input type="text"
                  className="form-control"
                  name="description"
                  placeholder="Description"
-                 ref="description" />
+                 ref="description"
+                 value={this.state.description}
+                 onChange={this.handleChange} />
         </div>
         <button type="submit" className="btn btn-primary">Add</button>
       </form>
